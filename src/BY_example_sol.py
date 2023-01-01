b4e45daa6d0f057c85e6fbb5b76965d1dd2e1e4f
lphansen <46974406+lphansen@users.noreply.github.com>
2022-12-31 19:37:59 -0800

    Add files via upload

--- src/BY_example_sol.py
+++ src/BY_example_sol.py
@@ -1,8 +1,10 @@
 import numpy as np
 import autograd.numpy as anp
 import scipy as sp
+from scipy import optimize
 np.set_printoptions(suppress=True)
-import pickle
+from tqdm import tqdm
+import pickle 
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -11,12 +13,6 @@ sns.set(font_scale = 1.5)
 import warnings
 warnings.filterwarnings("ignore")
 
-"""
-This Python script solves the Bansal Yaron Long-run risk model, computes and plots the shock elasticities, given specific parameter values. 
-
-Updated on Dec. 19, 2022, 1.08 A.M. CT
-"""
-
 from uncertain_expansion import uncertain_expansion
 from elasticity import exposure_elasticity, price_elasticity
 from lin_quad import LinQuadVar
@@ -24,7 +20,7 @@ from lin_quad_util import next_period
 
 def eq_cond_BY(X_t, X_tp1, W_tp1, q, *args):
     # Parameters for the model
-    γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π = args
+    γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π, plug_in = args
 
     # Variables:
     vmc_t, rmc_t, pd_t, x_t, σ_t_squared = X_t.ravel()
@@ -49,7 +45,7 @@ def eq_cond_BY(X_t, X_tp1, W_tp1, q, *args):
 
 def ss_func_BY(*args):
     # Extra parameters for the model
-    γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π = args
+    γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π, plug_in = args
 
     vmc = (np.log(1-β) - np.log(1-β*np.exp((1-ρ)*μ)))/(1-ρ)
     rmc = vmc + μ
@@ -59,33 +55,23 @@ def ss_func_BY(*args):
 
 def gc_tp1_approx(X_t, X_tp1, W_tp1, q, *args):
     # Parameters for the model
-    γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π = args
+    γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π, plug_in = args
 
     # Variables:
-    pd_t, x_t, σ_t_squared = X_t.ravel()
-    pd_tp1, x_tp1, σ_tp1_squared = X_tp1.ravel()
-    w1_tp1, w2_tp1, w3_tp1, w4_tp1 = W_tp1.ravel()
-    
+    if plug_in:
+        pd_t, x_t, σ_t_squared = X_t.ravel()
+        pd_tp1, x_tp1, σ_tp1_squared = X_tp1.ravel()
+        w1_tp1, w2_tp1, w3_tp1, w4_tp1 = W_tp1.ravel()
+    else:
+        vmc_t, rmc_t, pd_t, x_t, σ_t_squared = X_t.ravel()
+        vmc_tp1, rmc_tp1, pd_tp1, x_tp1, σ_tp1_squared = X_tp1.ravel()
+        w1_tp1, w2_tp1, w3_tp1, w4_tp1 = W_tp1.ravel()
     σ_t = anp.sqrt(σ_t_squared)
     
     gc_tp1 = μ + x_t + ϕ_c*σ_t*w3_tp1
     
     return gc_tp1
 
-def gc_tp1_approx0(X_t, X_tp1, W_tp1, q, *args):
-    # Parameters for the model
-    γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π = args
-
-    # Variables:
-    vmc_t, rmc_t, pd_t, x_t, σ_t_squared = X_t.ravel()
-    vmc_tp1, rmc_tp1, pd_tp1, x_tp1, σ_tp1_squared = X_tp1.ravel()
-    w1_tp1, w2_tp1, w3_tp1, w4_tp1 = W_tp1.ravel()
-    
-    σ_t = anp.sqrt(σ_t_squared)
-    
-    gc_tp1 = μ + x_t + ϕ_c*σ_t*w3_tp1
-    
-    return gc_tp1
 
 def calc_SDF(res):
 
@@ -109,6 +95,7 @@ def solve_BY(ρ= 2./3):
 
     γ = 10
     β = .998
+    # ρ = 2./3
     α = 0.979
     ϕ_e = 0.044 * σ_original
     σ_squared = 1.0
@@ -121,9 +108,10 @@ def solve_BY(ρ= 2./3):
     ϕ_d = 4.5 * σ_original
     π = 0.0
 
-    args = (γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π)
+    plug_in = False
+    args = (γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π, plug_in)
 
-    ModelSol = uncertain_expansion(eq_cond_BY, ss_func_BY, (1, 2, 4), args, [gc_tp1_approx0], [gc_tp1_approx], adj_loc_list = [], tol = 1e-8, max_iter = 50)
+    ModelSol = uncertain_expansion(eq_cond_BY, ss_func_BY, (1, 2, 4), args, [gc_tp1_approx], adj_loc_list = [], endo_loc_list = [], init_N = 'N0', util_adj = False, tol = 1e-8, max_iter = 50)
 
     res = {'Z1_tp1':ModelSol['Z1_tp1'],\
             'Z2_tp1':ModelSol['Z2_tp1'],\
@@ -142,18 +130,38 @@ def solve_BY(ρ= 2./3):
 # res_015 = solve_BY(ρ= 1.5)
 # res_100 = solve_BY(ρ= 10)
 
+# with open('data/res_006.pkl', 'wb') as f:
+#     pickle.dump(res_006,f)
+# with open('data/res_010.pkl', 'wb') as f:
+#     pickle.dump(res_010,f)
+# with open('data/res_015.pkl', 'wb') as f:
+#     pickle.dump(res_015,f)
+# with open('data/res_100.pkl', 'wb') as f:
+#     pickle.dump(res_100,f)
+
 def solve_BY_elas(γ=10, β=.998, ρ=2./3, α = 0.979, ϕ_e = 0.044*0.0078, ν_1=0.987, σ_w = 0.23 * 1e-5 / 0.0078**2, μ=0.0015, ϕ_c=0.0078):
 
     σ_original = 0.0078
+
+    # γ = 10
+    # β = .998
+    # ρ = 2./3
+    # α = 0.979
+    # ϕ_e = 0.044 * σ_original
     σ_squared = 1.0
+    # ν_1 = 0.987
+    # σ_w = 0.23 * 1e-5 / σ_original**2
+    # μ = 0.0015
     μ_d = 0.0015
+    # ϕ_c = 1.0 * σ_original
     ϕ = 3.0
     ϕ_d = 4.5 * σ_original
     π = 0.0
-    
-    args = (γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π)
 
-    ModelSol = uncertain_expansion(eq_cond_BY, ss_func_BY, (1, 2, 4), args, [gc_tp1_approx0], [gc_tp1_approx], adj_loc_list = [], tol = 1e-8, max_iter = 50)
+    plug_in = False
+    args = (γ, β, ρ, α, ϕ_e, σ_squared, ν_1, σ_w, μ, μ_d, ϕ, ϕ_d, ϕ_c, π, plug_in)
+
+    ModelSol = uncertain_expansion(eq_cond_BY, ss_func_BY, (1, 2, 4), args, [gc_tp1_approx], adj_loc_list = [], endo_loc_list = [], init_N = 'N0', util_adj = False, tol = 1e-8, max_iter = 50)
 
     res = {'Z1_tp1':ModelSol['Z1_tp1'],\
             'Z2_tp1':ModelSol['Z2_tp1'],\
@@ -188,7 +196,9 @@ def solve_BY_elas(γ=10, β=.998, ρ=2./3, α = 0.979, ϕ_e = 0.044*0.0078, ν_1
 
     n_qt = len(quantile)
     plot_expo_elas = [plot_expo_elas_shock_0, plot_expo_elas_shock_2, plot_expo_elas_shock_1] 
+    print(np.max(abs(np.array([expo_elas_shock_0,expo_elas_shock_2,expo_elas_shock_1]))))
     plot_price_elas = [plot_price_elas_shock_0, plot_price_elas_shock_2, plot_price_elas_shock_1] 
+    print(np.max(abs(np.array([price_elas_shock_0,price_elas_shock_2,price_elas_shock_1]))))
     shock_name = ['growth shock', 'consumption shock', 'volatility shock']
     qt = ['0.25 quantile','0.5 quantile','0.75 quantile']
     colors = ['green','red','blue']
@@ -221,3 +231,22 @@ def solve_BY_elas(γ=10, β=.998, ρ=2./3, α = 0.979, ϕ_e = 0.044*0.0078, ν_1
     print('μ = '+str("{:.4g}".format(μ)))
     print('ϕ_c = '+str("{:.4g}".format(ϕ_c)))
     plt.show()
+
+    # return expo_elas_shock_0, expo_elas_shock_1, expo_elas_shock_2, price_elas_shock_0, price_elas_shock_1, price_elas_shock_2
+
+# i=0
+# for gamma in tqdm([5, 10, 20]):
+#     for beta in tqdm([0.995, 0.998, 0.999]):
+#         for rho in [2./3, 1.00001, 1.5, 10]:
+#             for mu in [0.001, 0.0015, 0.002]:
+#                 for phic in [0.007,0.0078,0.0086]:
+#                     name = '_'.join(['res','gamma',str("{:.4f}".format(gamma)).replace('.',''),\
+#                                         'beta',str("{:.4f}".format(beta)).replace('.',''),\
+#                                         'rho',str("{:.4f}".format(rho)).replace('.',''),\
+#                                         'mu',str("{:.4f}".format(mu)).replace('.',''),\
+#                                         'phic',str("{:.4f}".format(phic)).replace('.','')])
+#                     i+=1
+#                     print(i,name)
+#                     with open('data/'+name+'.pkl', 'wb') as f:
+#                         pickle.dump([solve_BY_elas(γ=gamma, β=beta, ρ=rho, μ=mu, ϕ_c=phic)],f)
+                    
